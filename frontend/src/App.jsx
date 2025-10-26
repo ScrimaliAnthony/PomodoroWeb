8622ee94037c6cd3a65a24d46b06aa1acb510b90
Scrimali Anthony <anthony.scrimali@gmail.com>
2025-10-26 09:15:40 +0100

    [feat] POM-008-sequence : Modification de la structure de donnée du timer pour en avoir plusieurs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,28 @@ import UpdatePomodoro from "./components/UpdatePomodoro";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
+  // const [minutes, setMinutes] = useState(25);
+  // const [seconds, setSeconds] = useState(0);
   const [selectedTime, setSelectedTime] = useState();
   const [isStart, setIsStart] = useState(false);
+  const [timer, setTimer] = useState([
+    { id: 0, label: "Concentration", minutes: 25, seconds: 0 },
+    { id: 1, label: "Pause", minutes: 5, seconds: 0 },
+    { id: 2, label: "Concentration", minutes: 25, seconds: 0 }
+  ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    setSelectedTime(minutes * 60 + seconds)
-  }, [minutes, seconds])
+    setSelectedTime(timer[currentIndex].minutes * 60 + timer[currentIndex].seconds)
+  }, [timer[currentIndex].minutes, timer[currentIndex].seconds]);
 
   return (
     <>
       <h1>Pomodoro</h1>
+      
       <PomodoroTimer selectedTime={selectedTime} isStart={isStart}/>
       <StartPausePomodoro isStart={isStart} setIsStart={setIsStart} />
-      <UpdatePomodoro setMinutes={setMinutes} setSeconds={setSeconds} />
+      {/* <UpdatePomodoro setMinutes={setMinutes} setSeconds={setSeconds} /> */}
     </>
   )
 }
