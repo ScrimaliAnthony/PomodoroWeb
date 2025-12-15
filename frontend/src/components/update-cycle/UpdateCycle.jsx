f1e29dade3a0a8ac7658013bf94eaf897a6016ea
ScrimaliAnthony <anthony.scrimali@gmail.com>
2025-12-15 13:18:42 +0100

    [bugfix] POM-105-bugfix : totalCycle ne peux plus être inférieur à cycle

--- frontend/src/components/update-cycle/UpdateCycle.jsx
+++
@@ -1,7 +0,0 @@
-export default function UpdateCycle({ nbCycleInput, setNbCycleInput, maxCycle }) {
-
-    return <div style={{display: "flex"}}>
-        <input type="number" min="0" placeholder="cycle" value={nbCycleInput} onChange={(e) => setNbCycleInput(e.target.value)}/>
-        <p style={{fontWeight: "bold"}}>/ {maxCycle}</p>
-    </div>
-}
