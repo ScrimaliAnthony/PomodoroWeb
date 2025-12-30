12707ba4d2b076fd15ad1e93f00f857d98699677
ScrimaliAnthony <anthony.scrimali@gmail.com>
2025-12-30 12:57:45 +0100

    [fix] develop : modification du initialTask

--- frontend/src/data/tasks.js
+++ frontend/src/data/tasks.js
@@ -1,7 +1,7 @@
 import { TASK_STATUS } from "../constants/tasks";
 
 export const initialTasks = [
-    { id: 0, title: "My first Task", status: TASK_STATUS.DONE, desc: "Add my today tasks to the list", actualCycle: 0, nbCycle: 1, isDone: true },
+    { id: 0, title: "My first Task", status: TASK_STATUS.DONE, desc: "Add my today tasks to the list", actualCycle: 1, nbCycle: 1, isDone: true },
     { id: 1, title: "My second Task", status: TASK_STATUS.IN_PROGRESS, desc: "Add a task to the cycle", actualCycle: 0, nbCycle: 2, isDone: false },
-    { id: 2, title: "My third Task", status: TASK_STATUS.TODO, desc: "Finish my today tasks", actualCycle: 1, nbCycle: 1, isDone: false }
+    { id: 2, title: "My third Task", status: TASK_STATUS.TODO, desc: "Finish my today tasks", actualCycle: 0, nbCycle: 1, isDone: false }
 ];
